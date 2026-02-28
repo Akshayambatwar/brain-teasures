997e01cea9d2ba1a75068d1460da1b44019e404e
Akshayambatwar <ambatwarakshay@gmail.com>
2026-02-28 08:43:32 +0530

    commetn code

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -23,9 +23,9 @@ export async function POST(req) {
       .update(rawBody)
       .digest("hex");
 
-    if (webhookSignature !== expectedSignature) {
-      return NextResponse.json({ success: false }, { status: 400 });
-    }
+    // if (webhookSignature !== expectedSignature) {
+    //   return NextResponse.json({ success: false }, { status: 400 });
+    // }
 
     const event = JSON.parse(rawBody);
 
